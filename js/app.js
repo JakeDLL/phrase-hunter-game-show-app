07992495b087d0c8ee2d5f50fd15802211981eea
Jacob D <35861176+JakeDLL@users.noreply.github.com>
2020-10-06 20:02:11 -0400

    reset project
    Something was not working then everything wasn't working

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
-/* Treehouse FSJS Techdegree
+/* 
+ * Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * app.js */
-
-const startButton = document.querySelector('#btn__reset');
-startButton.addEventListener('click', () => {
-    const game = new Game();
-    game.startGame();
-    const keyboard = document.querySelectorAll('.key');
-    keyboard.forEach(key => key.addEventListener('click', letter => game.handleInteraction(letter)));
-});
+ * app.js 
+ * */
